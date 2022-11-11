9900f1398a345185cf5cc7068908d01315f5eeb3
daniel <daniel.marques@edu.unifil.br>
2022-11-11 14:18:04 -0300

    project created

---
+++ src/main/java/com/lanchonete/lanchoneteSpring/LanchoneteSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lanchonete.lanchoneteSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanchoneteSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanchoneteSpringApplication.class, args);
+	}
+
+}
