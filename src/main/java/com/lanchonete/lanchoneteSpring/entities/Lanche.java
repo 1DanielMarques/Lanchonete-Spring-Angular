143e01b2e54284f6c047d29d24e82bba89c732c3
daniel <daniel.marques@edu.unifil.br>
2022-11-11 14:32:00 -0300

    entidade Lanche

---
+++ src/main/java/com/lanchonete/lanchoneteSpring/entities/Lanche.java
@@ -0,0 +1,75 @@
+package com.lanchonete.lanchoneteSpring.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_lanche")
+public class Lanche implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private double preco;
+    private String descricao;
+
+    public Lanche() {
+
+    }
+
+    public Lanche(Long id, String nome, double preco, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lanche lanche = (Lanche) o;
+        return id.equals(lanche.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
