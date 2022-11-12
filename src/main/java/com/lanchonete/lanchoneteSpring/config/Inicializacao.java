c51a5ce1a86b0e79a28d505a3da51e9aba845642
daniel <daniel.marques@edu.unifil.br>
2022-11-12 17:09:00 -0300

    atributo sabor e CRUD funcionando

--- src/main/java/com/lanchonete/lanchoneteSpring/config/Inicializacao.java
+++ src/main/java/com/lanchonete/lanchoneteSpring/config/Inicializacao.java
@@ -1,6 +1,8 @@
 package com.lanchonete.lanchoneteSpring.config;
 
+import com.lanchonete.lanchoneteSpring.entities.Bebida;
 import com.lanchonete.lanchoneteSpring.entities.Lanche;
+import com.lanchonete.lanchoneteSpring.repositories.IBebidaRepository;
 import com.lanchonete.lanchoneteSpring.repositories.ILancheRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,12 +16,19 @@ public class Inicializacao implements CommandLineRunner {
     @Autowired
     private ILancheRepository lancheRepository;
 
+    @Autowired
+    private IBebidaRepository bebidaRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
         Lanche l1 = new Lanche(null, "X-Frango", 10.0, "Descricao do X-Frango");
         Lanche l2 = new Lanche(null, "X-Bacon", 16.55, "Descricao do X-Bacon");
 
+        Bebida b1 = new Bebida(null, "Refrigerante", "Coca-Cola", "2L", "Cola", 12.55);
+        Bebida b2 = new Bebida(null, "Suco", "Natu", "1L", "Laranja", 8.00);
+
         lancheRepository.saveAll(Arrays.asList(l1, l2));
+        bebidaRepository.saveAll(Arrays.asList(b1, b2));
     }
 }
--- src/main/java/com/lanchonete/lanchoneteSpring/entities/Bebida.java
+++ src/main/java/com/lanchonete/lanchoneteSpring/entities/Bebida.java
@@ -1,13 +1,11 @@
 package com.lanchonete.lanchoneteSpring.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_bebida")
 public class Bebida implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -17,17 +15,19 @@ public class Bebida implements Serializable {
     private String nome;
     private String marca;
     private String litragem;
+    private String sabor;
     private double preco;
 
     public Bebida() {
 
     }
 
-    public Bebida(Long id, String nome, String marca, String litragem, double preco) {
+    public Bebida(Long id, String nome, String marca, String litragem, String sabor, double preco) {
         this.id = id;
         this.nome = nome;
         this.marca = marca;
         this.litragem = litragem;
+        this.sabor = sabor;
         this.preco = preco;
     }
 
@@ -61,6 +61,14 @@ public class Bebida implements Serializable {
 
     public void setLitragem(String litragem) {
         this.litragem = litragem;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
     }
 
     public double getPreco() {
