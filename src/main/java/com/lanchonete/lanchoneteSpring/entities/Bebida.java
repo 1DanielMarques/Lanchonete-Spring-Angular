4756c68ca8768bb78e634f0a5189fc209de7e1e0
daniel <daniel.marques@edu.unifil.br>
2022-11-12 16:59:10 -0300

    entidade bebida

---
+++ src/main/java/com/lanchonete/lanchoneteSpring/entities/Bebida.java
@@ -0,0 +1,79 @@
+package com.lanchonete.lanchoneteSpring.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Bebida implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nome;
+    private String marca;
+    private String litragem;
+    private double preco;
+
+    public Bebida(){
+
+    }
+
+    public Bebida(Long id, String nome, String marca, String litragem, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.marca = marca;
+        this.litragem = litragem;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getLitragem() {
+        return litragem;
+    }
+
+    public void setLitragem(String litragem) {
+        this.litragem = litragem;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bebida bebida = (Bebida) o;
+        return id.equals(bebida.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
