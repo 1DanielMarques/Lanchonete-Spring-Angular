f8824a13dc25af96c4fe43a67937055ddc4d731e
daniel <daniel.marques@edu.unifil.br>
2022-11-13 18:37:02 -0300

    entidade Pedido

---
+++ src/main/java/com/lanchonete/lanchoneteSpring/entities/Pedido.java
@@ -0,0 +1,89 @@
+package com.lanchonete.lanchoneteSpring.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_pedido")
+public class Pedido implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Cliente cliente;
+    private List<Lanche> lanches = new ArrayList<>();
+    private List<Bebida> bebidas = new ArrayList<>();
+    private int qtdLanches;
+    private int qtdBebidas;
+
+    public Pedido() {
+
+    }
+
+    public Pedido(Long id, Cliente cliente, List<Lanche> lanches, List<Bebida> bebidas, int qtdLanches, int qtdBebidas) {
+        this.id = id;
+        this.cliente = cliente;
+        this.lanches = lanches;
+        this.bebidas = bebidas;
+        this.qtdLanches = qtdLanches;
+        this.qtdBebidas = qtdBebidas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Lanche> getLanches() {
+        return lanches;
+    }
+
+    public List<Bebida> getBebidas() {
+        return bebidas;
+    }
+
+    public int getQtdLanches() {
+        return qtdLanches;
+    }
+
+    public void setQtdLanches(int qtdLanches) {
+        this.qtdLanches = qtdLanches;
+    }
+
+    public int getQtdBebidas() {
+        return qtdBebidas;
+    }
+
+    public void setQtdBebidas(int qtdBebidas) {
+        this.qtdBebidas = qtdBebidas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pedido pedido = (Pedido) o;
+        return id.equals(pedido.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
