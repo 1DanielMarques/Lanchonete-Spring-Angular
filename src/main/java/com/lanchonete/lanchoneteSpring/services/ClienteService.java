5c3e14798e7fa66c324d0ea177551ebefc2e535e
daniel <daniel.marques@edu.unifil.br>
2022-11-14 14:40:24 -0300

    ClienteService

---
+++ src/main/java/com/lanchonete/lanchoneteSpring/services/ClienteService.java
@@ -0,0 +1,49 @@
+package com.lanchonete.lanchoneteSpring.services;
+
+import com.lanchonete.lanchoneteSpring.entities.Cliente;
+import com.lanchonete.lanchoneteSpring.repositories.IClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    IClienteRepository repository;
+
+    public Cliente insert(Cliente obj) {
+        return repository.save(obj);
+    }
+
+    public List<Cliente> findAll() {
+        return repository.findAll();
+    }
+
+    public Cliente findById(Long id) {
+        Optional<Cliente> obj = repository.findById(id);
+        return obj.orElseThrow();
+    }
+
+    public void delete(Long id) {
+        Cliente obj = findById(id);
+        repository.delete(obj);
+    }
+
+    public Cliente update(Long id, Cliente obj) {
+        Cliente c1 = findById(id);
+        c1 = updateData(c1, obj);
+        return repository.save(c1);
+    }
+
+    private Cliente updateData(Cliente c1, Cliente c2) {
+        c1.setNome(c2.getNome());
+        c1.setEndereco(c2.getEndereco());
+        c1.setTelefone(c2.getTelefone());
+        c1.setAniversario(c2.getAniversario());
+        return c1;
+    }
+
+}
