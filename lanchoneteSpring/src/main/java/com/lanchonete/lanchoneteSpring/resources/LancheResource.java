d90095f7c2259c21fca21bb62c8e024fe6856c44
daniel <daniel.marques@edu.unifil.br>
2022-12-09 15:50:57 -0300

    alterando endpoint api/lanches

--- lanchoneteSpring/src/main/java/com/lanchonete/lanchoneteSpring/resources/LancheResource.java
+++ lanchoneteSpring/src/main/java/com/lanchonete/lanchoneteSpring/resources/LancheResource.java
@@ -12,7 +12,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/lanches")
+@RequestMapping(value = "/api/lanches")
 public class LancheResource {
 
     @Autowired
