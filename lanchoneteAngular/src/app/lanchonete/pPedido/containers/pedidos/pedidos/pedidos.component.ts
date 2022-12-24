39605dd110787523d22254cfa689eda7bec3bd15
Daniel Marques <daniel.marques@edu.unifil.br>
2022-12-24 16:20:33 -0300

    matDialog onError

--- lanchoneteAngular/src/app/lanchonete/pPedido/containers/pedidos/pedidos/pedidos.component.ts
+++ lanchoneteAngular/src/app/lanchonete/pPedido/containers/pedidos/pedidos/pedidos.component.ts
@@ -1,5 +1,7 @@
+import { ErrorDialogComponent } from './../../../../../shared/components/error-dialog/error-dialog.component';
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, Observable, of } from 'rxjs';
 import { Pedido } from 'src/app/lanchonete/model/pedido';
 import { PedidoService } from 'src/app/lanchonete/services/pedido/pedido.service';
@@ -15,7 +17,7 @@ import { LancheService } from './../../../../services/lanche/lanche.service';
 })
 export class PedidosComponent {
 
-  constructor(private lancheService: LancheService, private bebidaService: BebidaService, private pedidoService: PedidoService, private router: Router, private route: ActivatedRoute) {
+  constructor(private lancheService: LancheService, private bebidaService: BebidaService, private pedidoService: PedidoService, private router: Router, private route: ActivatedRoute, public dialog: MatDialog) {
     this.refresh();
   }
 
@@ -26,11 +28,17 @@ export class PedidosComponent {
       pipe(
         catchError(
           () => {
-            console.log('erro');
+            this.onError('Erro ao carregar Pedidos.')
             return of([])
           }
         )
       );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
   }
 
   onHome() {
